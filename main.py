91a1e1aac15b9207495d433ceb495b09f7d56600
Esteban D. 'K1d' Barboza Primera <dario0117@gmail.com>
2017-09-28 16:41:03 -0500

    Refactorización y agregadas las plantillas para cada opción

--- main.py
+++ main.py
@@ -3,7 +3,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/list')
+def lst():
+    return render_template('list.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+
 
 if __name__ == '__main__':
     app.run(debug= True, port=80)
