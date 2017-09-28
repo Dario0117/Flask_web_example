db8f65cc0246d817d8037c1fb2a421b3cf2d515b
Esteban D. 'K1d' Barboza Primera <dario0117@gmail.com>
2017-09-28 17:02:59 -0500

    Agregado el "toString" para la entidad

--- back/Student.py
+++ back/Student.py
@@ -5,3 +5,6 @@ class Student:
         self.lastname = lastname
         self.age = age
         self.email = email
+
+    def __str__(self):
+        return "%s %s %d %s" % (self.name, self.lastname, self.age, self.email)
