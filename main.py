1e0748a4dfe62318c2a28969698332e2511dfcfa
Esteban D. 'K1d' Barboza Primera <dario0117@gmail.com>
2017-09-28 19:46:35 -0500

    Buscar: Funcionando

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ def edit(id):
     if len(student) == 0: return render_template('list.html', students=students)
     return render_template('edit.html', student=student[0])
 
-@app.route('/delete/<int:id>', methods=['GET'])
+@app.route('/delete/<int:id>')
 def delete(id):
     student = [x for x in students if x.id == id]
     students.remove(student[0])
     return "x"
 
+@app.route('/search/', methods=['GET'])
+def search():
+    name = request.args.get('name')
+    students_filt = [x for x in students if name in x.name.lower() or name.lower() in x.lastname.lower()]
+    return render_template('home.html', students=students_filt, home=True)
+
 if __name__ == '__main__':
-    students.append(Student("Esteban 1", "Barboza 1", 22, "e@mail.com"))
+    students.append(Student("steban 1", "Barboza 1", 22, "e@mail.com"))
     students.append(Student("Esteban 2", "Barboza 2", 22, "e@mail.com"))
     students.append(Student("Esteban 3", "Barboza 3", 22, "e@mail.com"))
     students.append(Student("Esteban 4", "Barboza 4", 22, "e@mail.com"))
